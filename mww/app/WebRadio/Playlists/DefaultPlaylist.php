73958262353a17a83ec77084439ea11e63067ed3
Lucas Bustamante <lucasfbustamante@gmail.com>
2019-01-28 00:46:06 -0200

    Added "John Denver - Take Me Home, Country Roads"

--- mww/app/WebRadio/Playlists/DefaultPlaylist.php
+++ mww/app/WebRadio/Playlists/DefaultPlaylist.php
@@ -35,6 +35,7 @@ class DefaultPlaylist implements PlaylistInterface {
             'Gilberto Gil e Milton Nascimento - Sebastião' => 'https://www.youtube.com/watch?v=xUCJd1au5AM',
             'Barrington Levy - Black Roses' => 'https://www.youtube.com/watch?v=OdhEAl_sI1A',
             'Rupert Holmes - Escape' => 'https://www.youtube.com/watch?v=TazHNpt6OTo',
+            'John Denver - Take Me Home, Country Roads' => 'https://www.youtube.com/watch?v=1vrEljMfXYo',
         ];
     }
 }
